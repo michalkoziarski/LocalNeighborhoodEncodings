f403401c44210d331689ed4e51aba4c540325217
michalkoziarski <michal.koziarski@gmail.com>
2022-04-23 20:13:19 +0200

    added scheduler

---
+++ schedule_lne.py
@@ -0,0 +1,8 @@
+import os
+
+if __name__ == "__main__":
+    for fold in range(10):
+        for k in [2, 3, 4, 5, 6, 7]:
+            command = f"sbatch run.sh run_lne.py -fold {fold} -k {k}"
+
+            os.system(command)
