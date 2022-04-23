d32d8fa6aafa8f27e60e67dc91f769a5df7d7097
michalkoziarski <michal.koziarski@gmail.com>
2022-04-23 20:17:28 +0200

    fixed script name

--- schedule_lne.py
+++ schedule_lne.py
@@ -3,6 +3,6 @@ import os
 if __name__ == "__main__":
     for fold in range(10):
         for k in [2, 3, 4, 5, 6, 7]:
-            command = f"sbatch run.sh run_lne.py -fold {fold} -k {k}"
+            command = f"sbatch slurm.sh run_lne.py -fold {fold} -k {k}"
 
             os.system(command)
