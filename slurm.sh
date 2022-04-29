1f37d7750a74cead504914e70cde7315e2a2f499
michalkoziarski <michal.koziarski@gmail.com>
2022-04-29 14:28:28 +0200

    updated slurm script

--- slurm.sh
+++ slurm.sh
@@ -3,8 +3,8 @@
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=72:00:00
-#SBATCH -A plgimba2
+#SBATCH -A plgimba3-cpu
 
-module add plgrid/tools/python/3.9
+module add python/3.9.6-gcccore-11.2.0
 
 python3 -W ignore ${1} ${@:2}
