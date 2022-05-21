b971828a8fb77eaaf586d702b6aaf718ddeb10c1
michalkoziarski <michal.koziarski@gmail.com>
2022-05-21 15:24:37 +0200

    excluded ROS LNE trials

--- schedule_lne.py
+++ schedule_lne.py
@@ -10,17 +10,7 @@ if __name__ == "__main__":
                     f"sbatch slurm.sh run_lne.py "
                     f"-classifier_name {classifier_name} "
                     f"-oversampler_name smote "
-                    f"-fold {fold} -k {k} -eps {0.0}"
+                    f"-fold {fold} -k {k}"
                 )
 
                 os.system(command)
-
-                for eps in [0.0, 0.01, 0.05, 0.1]:
-                    command = (
-                        f"sbatch slurm.sh run_lne.py "
-                        f"-classifier_name {classifier_name} "
-                        f"-oversampler_name ros "
-                        f"-fold {fold} -k {k} -eps {eps}"
-                    )
-
-                    os.system(command)
