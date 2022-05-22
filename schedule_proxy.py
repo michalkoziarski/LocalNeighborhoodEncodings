c9c6a99a1f3b9c63d838a0c9ed0ef41f562d3bad
michalkoziarski <michal.koziarski@gmail.com>
2022-05-22 14:36:48 +0200

    simplified scheduler

--- schedule_proxy.py
+++ schedule_proxy.py
@@ -2,13 +2,11 @@ import os
 
 if __name__ == "__main__":
     for fold in range(10):
-        for classifier_name in ["MLP"]:
-            for k in [4]:
-                command = (
-                    f"sbatch slurm.sh run_proxy.py "
-                    f"-classifier_name {classifier_name} "
-                    f"-oversampler_name smote "
-                    f"-fold {fold} -k {k}"
-                )
+        command = (
+            f"sbatch slurm.sh run_proxy.py "
+            f"-classifier_name MLP "
+            f"-oversampler_name smote "
+            f"-fold {fold} -k 4"
+        )
 
-                os.system(command)
+        os.system(command)
